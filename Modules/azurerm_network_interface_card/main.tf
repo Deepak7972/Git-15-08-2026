34ba4feb4633dfbe45864358e3edb18aef5121b4
Deepak7972 <nathdeepak193@gmail.com>
2026-08-20 00:32:53 +0530

    removed the data from infra.yml

--- Modules/azurerm_network_interface_card/main.tf
+++ Modules/azurerm_network_interface_card/main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_network_interface" "nic1" {
-  for_each = var.network_interface_card
-  name     = each.value.name
-  location = each.value.location
+  for_each            = var.network_interface_card
+  name                = each.value.name
+  location            = each.value.location
   resource_group_name = each.value.resource_group_name
 
   ip_configuration {
-    name = each.value.ip_config_name
+    name                 = each.value.ip_config_name
     public_ip_address_id = data.azurerm_public_ip.pip1[each.key].id
-    subnet_id = data.azurerm_subnet.subnet1[each.key].id
+    subnet_id            = data.azurerm_subnet.subnet1[each.key].id
 
     private_ip_address_allocation = "Dynamic"
   }
@@ -15,14 +15,14 @@ resource "azurerm_network_interface" "nic1" {
 
 
 resource "azurerm_linux_virtual_machine" "vm1" {
-  for_each = var.network_interface_card
-  name                = each.value.vm_name
-  resource_group_name = each.value.resource_group_name
-  location            = each.value.location
-  size                = each.value.size
-  admin_username      = each.value.admin_username
-  network_interface_ids = [ azurerm_network_interface.nic1[each.key].id ]
-  
+  for_each              = var.network_interface_card
+  name                  = each.value.vm_name
+  resource_group_name   = each.value.resource_group_name
+  location              = each.value.location
+  size                  = each.value.size
+  admin_username        = each.value.admin_username
+  network_interface_ids = [azurerm_network_interface.nic1[each.key].id]
+
   custom_data = filebase64("${path.module}/cloud-init.yaml")
 
   disable_password_authentication = false
